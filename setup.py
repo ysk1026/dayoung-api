9cd46d50c7b591f067738fd9be393660f137b2b6
Kim <fkqoseka@gmail.com>
2020-10-24 23:50:01 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+# pip install .
+with open("README.py", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='com_dayoung_api',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='youngseonkim',
+    author_email='fkqoseka@gmail.com',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=setuptools.find_packages(),
+    python_requires= '>= 3.7'
+)
