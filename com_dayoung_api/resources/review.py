0d4d7dacca55469aeec3bed53a34db7f3f868116
Kim <fkqoseka@gmail.com>
2020-10-27 10:13:37 +0900

    dao should be writen

--- com_dayoung_api/resources/review.py
+++ com_dayoung_api/resources/review.py
@@ -1,19 +1,27 @@
 from flask_restful import Resource, reqparse
+from flask import request
+import json
+from flask import jsonify
+import pandas as pd
+import numpy as np
+import os
+from com_dayoung_api.util.file import FileReader
+from pathlib import Path
 from com_dayoung_api.ext.db import db, openSession
-from com_dayoung_api.resources.user import UserDto
-from com_dayoung_api.resources.movie import MovieDto
+# from com_dayoung_api.resources.user import UserDto
+# from com_dayoung_api.resources.movie import MovieDto
 
 class ReviewDto(db.Model):
     __tablename__ = "reviews"
     __table_args__ = {'mysql_collate':'utf8_general_ci'}
     
-    rev_id: int = db.Column(db.Integer, primary_key=)
+    rev_id: int = db.Column(db.Integer, primary_key=True, index=True)
     title: str = db.Column(db.String(100))
     content: str = db.Column(db.String(500))
     label: int = db.Column(db.Integer(10))
      
-    user_id: str = db.Column(db.String(30), db.ForeignKey(UserDto.user_id))
-    movie_id: int = db.Column(db.Integer, db.ForeignKey(MovieDto.movie_id))
+    user_id: str = db.Column(db.String(30)) # db.ForeignKey(UserDto.user_id
+    movie_id: int = db.Column(db.Integer(30)) # db.ForeignKey(MovieDto.movie_id)
     
     def __init__(self, title, content, label, user_id, movie_id):
         self.title = title
@@ -33,9 +41,57 @@ class ReviewDto(db.Model):
             'user_id' : self.user_id,
             'movie_id' : self.movie_id,
             'title' : self.title,
-            'content' : self.content
+            'content' : self.content,
+            'label' : self.label
         }
         
+class ReviewVo:
+    rev_id: int = 0
+    title: str = ''
+    content: str = ''
+    label: int = 0
+    user_id: str = ''
+    movie_id: int = 0
+    
+
+# ==============================================================
+# ==============================================================
+# ====================     Service  ============================
+# ==============================================================
+# ==============================================================
+
+class ReviewService:
+    def __init__(self):
+        self.fileReader = FileReader()
+        self.data = os.path.join(os.path.abspath(os.path.dirname(__file__)) + '/data')
+    
+    def hook(self):
+        train = 'ratings.csv'
+        this = self.fileReader
+        this.train = self.new_model(train) # payload
+        
+        df = pd.DataFrame(
+            {
+                'user_id' : this.train.id,
+                'movie_id' : '1',
+                'title' : 'Avengers',
+                'content' : this.train.document,
+                'label' : this.train.label
+            }
+        )
+        
+        print(df.head())
+        
+        return df
+    
+    def new_model(self, payload) -> object:
+        this = self.fileReader
+        this.data = self.data
+        this.fname = payload
+        print(f'{self.data}')
+        print(f'{this.fname}')
+        return pd.read_csv(Path(self.data, this.fname))
+                
 class ReviewDao(ReviewDto):
     
     @classmethod
@@ -123,5 +179,6 @@ class Reviews(Resource):
         return {'articles' : list(map(lambda article: article.json(), ReviewDao.find_all()))}
         # return {'articles':[article.json() for article in ArticleDao.find_all()]}
         
-        
-        
+rv = ReviewService()
+rv.hook()
+    
